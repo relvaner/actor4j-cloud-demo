63911d666590bea2cf87344dc3066d949ab05012
relvaner <david4bauer@googlemail.com>
2024-07-23 20:47:24 +0200

    Refactoring

--- actor4j-cloud-demo-backend/actor4j-cloud-demo-backend-utils/src/main/java/io/actor4j/cloud/demo/backend/utils/Utils.java
+++ actor4j-cloud-demo-backend/actor4j-cloud-demo-backend-utils/src/main/java/io/actor4j/cloud/demo/backend/utils/Utils.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.actor4j.cloud.demo.module.backend.utils;
+package io.actor4j.cloud.demo.backend.utils;
 
 import java.util.ArrayList;
 import java.util.HashSet;
